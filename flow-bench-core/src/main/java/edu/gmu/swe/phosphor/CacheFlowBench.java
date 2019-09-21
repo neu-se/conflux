24217265899253661b8fb84cf63894245696fc6a
katherine-hough <khough2@gmu.edu>
2019-09-21 15:39:25 -0400

    * Changed cache test to be multilabel * Added BufferCache test

--- flow-bench-core/src/main/java/edu/gmu/swe/phosphor/CacheFlowBench.java
+++ flow-bench-core/src/main/java/edu/gmu/swe/phosphor/CacheFlowBench.java
@@ -2,12 +2,18 @@ package edu.gmu.swe.phosphor;
 
 import com.opensymphony.xwork2.ognl.OgnlUtil;
 import edu.columbia.cs.psl.phosphor.runtime.MultiTainter;
-import edu.gmu.swe.phosphor.ignored.runtime.BinaryFlowBenchResult;
+import edu.gmu.swe.phosphor.ignored.runtime.MultiLabelFlowBenchResult;
 import ognl.ASTConst;
 import ognl.ASTProperty;
 import ognl.OgnlException;
 import org.apache.juli.DateFormatCache;
+import org.mortbay.io.Buffer;
+import org.mortbay.io.BufferCache;
 import sun.util.locale.BaseLocale;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static edu.gmu.swe.phosphor.FlowBenchUtil.taintWithIndices;
 
@@ -20,21 +26,21 @@ public class CacheFlowBench {
      * Compiles OGNL expressions using com.opensymphony.xwork2.ognl.OgnlUtil which caches OGNL expressions.
      */
     @FlowBench
-    public void testOgnlExpressionCache(BinaryFlowBenchResult benchResult) throws OgnlException {
+    public void testOgnlExpressionCache(MultiLabelFlowBenchResult benchResult) throws OgnlException {
         String expression = "toString";
         String taintedExpression = taintWithIndices(expression);
         // Tainted value put into cache first
         OgnlUtil ognl1 = new OgnlUtil();
-        ASTConst compiledConst = (ASTConst)((ASTProperty) ognl1.compile(taintedExpression)).jjtGetChild(0);
-        benchResult.checkNonEmpty(compiledConst.getValue());
-        compiledConst = (ASTConst)((ASTProperty) ognl1.compile(expression)).jjtGetChild(0);
-        benchResult.checkEmpty(compiledConst.getValue());
+        ASTConst taintedCompiledConst1 = (ASTConst)((ASTProperty) ognl1.compile(taintedExpression)).jjtGetChild(0);
+        benchResult.check(IntStream.range(0, expression.length()).boxed().collect(Collectors.toList()), taintedCompiledConst1.getValue());
+        ASTConst compiledConst1 = (ASTConst)((ASTProperty) ognl1.compile(expression)).jjtGetChild(0);
+        benchResult.checkEmpty(compiledConst1.getValue());
         // Non-tainted value put into cache first
         OgnlUtil ognl2 = new OgnlUtil();
-        compiledConst = (ASTConst)((ASTProperty) ognl2.compile(expression)).jjtGetChild(0);
-        benchResult.checkEmpty(compiledConst.getValue());
-        compiledConst = (ASTConst)((ASTProperty) ognl2.compile(taintedExpression)).jjtGetChild(0);
-        benchResult.checkNonEmpty(compiledConst.getValue());
+        ASTConst compiledConst2 = (ASTConst)((ASTProperty) ognl2.compile(expression)).jjtGetChild(0);
+        benchResult.checkEmpty(compiledConst2.getValue());
+        ASTConst taintedCompiledConst2 = (ASTConst)((ASTProperty) ognl2.compile(taintedExpression)).jjtGetChild(0);
+        benchResult.check(IntStream.range(0, expression.length()).boxed().collect(Collectors.toList()), taintedCompiledConst2.getValue());
     }
 
     /**
@@ -42,28 +48,30 @@ public class CacheFlowBench {
      * sun.util.locale.BaseLocale also interns BaseLocales' fields when constructing them.
      */
     @FlowBench
-    public void testBaseLocaleCache(BinaryFlowBenchResult benchResult) {
+    public void testBaseLocaleCache(MultiLabelFlowBenchResult benchResult) {
         // Tainted value put into cache first
-        BaseLocale taintedZLocale = BaseLocale.getInstance(taintWithIndices("zzzzzzzz"),"ZZ", "", "");
-        benchResult.checkNonEmpty(taintedZLocale.getLanguage());
-        BaseLocale zLocale = BaseLocale.getInstance("zzzzzzzz", "ZZ", "", "");
+        String lang1 = "zzzzzzzz";
+        BaseLocale taintedZLocale = BaseLocale.getInstance(taintWithIndices(lang1),"ZZ", "", "");
+        benchResult.check(IntStream.range(0, lang1.length()).boxed().collect(Collectors.toList()), taintedZLocale.getLanguage());
+        BaseLocale zLocale = BaseLocale.getInstance(lang1, "ZZ", "", "");
         benchResult.checkEmpty(zLocale.getLanguage());
         // Non-tainted value put into cache first
-        BaseLocale qLocale = BaseLocale.getInstance("qqqqqqqq", "QQ", "", "");
+        String lang2 = "qqqqqqqq";
+        BaseLocale qLocale = BaseLocale.getInstance(lang2, "QQ", "", "");
         benchResult.checkEmpty(qLocale.getLanguage());
-        BaseLocale taintedQLocale = BaseLocale.getInstance(taintWithIndices("qqqqqqqq"), "QQ", "", "");
-        benchResult.checkNonEmpty(taintedQLocale.getLanguage());
+        BaseLocale taintedQLocale = BaseLocale.getInstance(taintWithIndices(lang2), "QQ", "", "");
+        benchResult.check(IntStream.range(0, lang2.length()).boxed().collect(Collectors.toList()), taintedQLocale.getLanguage());
     }
 
     /**
      * Formats longs as dates using org.apache.juli.DateFormatCache which caches formatted dates.
      */
     @FlowBench
-    public void testDateFormatCache(BinaryFlowBenchResult benchResult) {
+    public void testDateFormatCache(MultiLabelFlowBenchResult benchResult) {
         // Tainted value put into cache first
         DateFormatCache cache1 = new DateFormatCache(30, "dd-MMM-yyyy HH:mm:ss", null);
         String taintedDate1 = cache1.getFormat(MultiTainter.taintedLong(1000, 0));
-        benchResult.checkNonEmpty(taintedDate1);
+        benchResult.check(Collections.singletonList(0), taintedDate1);
         String date1 = cache1.getFormat(1000);
         benchResult.checkEmpty(date1);
         // Non-tainted value put into cache first
@@ -71,6 +79,29 @@ public class CacheFlowBench {
         String date2 = cache2.getFormat(55000);
         benchResult.checkEmpty(date2);
         String taintedDate2 = cache2.getFormat(MultiTainter.taintedLong(55000, 1));
-        benchResult.checkNonEmpty(taintedDate2);
+        benchResult.check(Collections.singletonList(1), taintedDate2);
+    }
+
+    /**
+     * Gets Buffers from org.mortbay.io.BufferCache instances which caches Buffer instances.
+     */
+    @FlowBench
+    public void testBufferCache(MultiLabelFlowBenchResult benchResult) {
+        // Tainted value cached
+        BufferCache cache1 = new BufferCache();
+        String value1 = "leo a diam sollicitudin";
+        cache1.add(taintWithIndices(value1), 0);
+        Buffer taintedBuffer1 = cache1.lookup(taintWithIndices(value1));
+        benchResult.check(IntStream.range(0, value1.length()).boxed().collect(Collectors.toList()), taintedBuffer1.asArray());
+        Buffer buffer1 = cache1.lookup(value1);
+        benchResult.checkEmpty(buffer1.asArray());
+        // Non-tainted value cached
+        String value2 = "cursus metus aliquam eleifend";
+        BufferCache cache2 = new BufferCache();
+        cache2.add(value2, 0);
+        Buffer buffer2 = cache2.lookup(value2);
+        benchResult.checkEmpty(buffer2.asArray());
+        Buffer taintedBuffer2 = cache2.lookup(taintWithIndices(value2));
+        benchResult.check(IntStream.range(0, value2.length()).boxed().collect(Collectors.toList()), taintedBuffer2.asArray());
     }
 }
