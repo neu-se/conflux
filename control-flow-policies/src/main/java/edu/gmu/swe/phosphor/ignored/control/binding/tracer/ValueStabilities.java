82bc987c32437bbbd19a928bd6d551ef4852d1e3
katherine-hough <khough2@gmu.edu>
2020-06-02 18:13:48 -0400

    * Removed outdated documentation

--- control-flow-policies/src/main/java/edu/gmu/swe/phosphor/ignored/control/binding/tracer/ValueStabilities.java
+++ control-flow-policies/src/main/java/edu/gmu/swe/phosphor/ignored/control/binding/tracer/ValueStabilities.java
@@ -15,31 +15,6 @@ import edu.gmu.swe.phosphor.ignored.control.ssa.expression.*;
 import edu.gmu.swe.phosphor.ignored.control.ssa.statement.AssignmentStatement;
 import edu.gmu.swe.phosphor.ignored.control.ssa.statement.Statement;
 
-/**
- * Calculates the loop-relative stability of a variable's (local variable or stack element) value.
- * <p>
- * Let m be a method in static single assignment form, s be an assignment statement in m which stores the value
- * of an expression e into the variable x_v (i.e., x_v := e), L1 be the set of natural loops in M, and L2 be the theoretical
- * set of loops which contain a particular call to m at runtime.
- * <p>
- * The value of x_v is considered to be stable relative to a some natural loop, l in L1 or l in L2, s is not contained in
- * l or along all paths consisting only of vertices contained in L starting from the header of L, x_v is either undefined
- * or its definition is always equal to the same value.
- * <p>
- * An expression e is considered to be stable with respect to a loop L if a one of the following conditions is met:
- * <ul>
- *     <li>e is a constant/literal</li>
- *     <li>e is of the form <i>a op b</i>, where op is a binary operator and the subexpressions a and b are both
- *     stable with respect to L </li>
- *     <li>e is of the form <i>op a</i>, where op is a unary operator and the subexpression a is stable with respect
- *     to L </li>
- *     <li>e is a variable whose value is stable with respect to L</li>
- * </ul>
- * If e is a memory allocation instruction (e.g., a new expression, or new array expression) then it is unstable
- * with respect to all loops that contain it.
- * If e is an array access, field access, phi function, or invoke expression, then we conservatively
- * say that e is unstable with respect to all loops that contain it.
- */
 public class ValueStabilities implements StatefulExpressionVisitor<LoopStability, Set<NaturalLoop<AnnotatedBasicBlock>>> {
 
     private final Map<VariableExpression, LoopStability> valueStabilityMap = new HashMap<>();
