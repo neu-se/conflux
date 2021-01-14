08364bb4c31bea59ad6550ec1d7ccc77d94b584e
katherine-hough <32645020+katherine-hough@users.noreply.github.com>
2021-01-13 19:54:34 -0500

    * still working on the openrefine study

--- conflux-experiments/experiments-openrefine/src/test/java/edu/neu/ccs/conflux/OpenRefineFlowStudy.java
+++ conflux-experiments/experiments-openrefine/src/test/java/edu/neu/ccs/conflux/OpenRefineFlowStudy.java
@@ -1,9 +1,12 @@
 package edu.neu.ccs.conflux;
 
+import com.google.refine.importers.SeparatorBasedImporter;
 import edu.neu.ccs.conflux.internal.FlowStudy;
 import edu.neu.ccs.conflux.internal.runtime.StudyTaintTagChecker;
 
+import java.io.StringReader;
 public class OpenRefineFlowStudy {
+
     /**
      * https://github.com/OpenRefine/OpenRefine/issues/2584
      */
@@ -12,12 +15,23 @@ public class OpenRefineFlowStudy {
         String input = FlowEvalUtil.readAndTaintResource(getClass(), "/openrefine-2583.csv");
         checker.recordInput(input);
         try {
-            String s = null;
-            s.length();
+            prepareOptions();
+            parseOneFile(new SeparatorBasedImporter(), new StringReader(input));
         } catch (Throwable t) {
             checker.check(t);
             return;
         }
         throw new AssertionError("Expected exception to be thrown");
+    }
+
+    private static void prepareOptions() {
+        whenGetStringOption("separator", options, ",");
+        whenGetIntegerOption("limit", options, -1);
+        whenGetIntegerOption("skipDataLines", options, 0);
+        whenGetIntegerOption("ignoreLines", options, 0);
+        whenGetIntegerOption("headerLines", options, 0);
+        whenGetBooleanOption("guessCellValueTypes", options, true);
+        whenGetBooleanOption("processQuotes", options, !false);
+        whenGetBooleanOption("trimStrings", options, true);
     }
 }
