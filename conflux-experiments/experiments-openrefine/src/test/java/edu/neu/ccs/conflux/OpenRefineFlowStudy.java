48767c62628c207dd4517771212dcf52f9163864
katherine-hough <32645020+katherine-hough@users.noreply.github.com>
2021-01-14 16:20:47 -0500

    * openrefine study

--- conflux-experiments/experiments-openrefine/src/test/java/edu/neu/ccs/conflux/OpenRefineFlowStudy.java
+++ conflux-experiments/experiments-openrefine/src/test/java/edu/neu/ccs/conflux/OpenRefineFlowStudy.java
@@ -3,7 +3,6 @@ package edu.neu.ccs.conflux;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.refine.ProjectMetadata;
 import com.google.refine.RefineServletStub;
-import com.google.refine.importers.ImportingParserBase;
 import com.google.refine.importers.SeparatorBasedImporter;
 import com.google.refine.importing.ImportingJob;
 import com.google.refine.importing.ImportingManager;
@@ -12,7 +11,6 @@ import com.google.refine.util.ParsingUtilities;
 import edu.columbia.cs.psl.phosphor.runtime.MultiTainter;
 import edu.neu.ccs.conflux.internal.FlowStudy;
 import edu.neu.ccs.conflux.internal.runtime.StudyTaintTagChecker;
-import org.mockito.Mockito;
 
 import java.io.StringReader;
 import java.util.ArrayList;
@@ -41,18 +39,16 @@ public class OpenRefineFlowStudy {
         RefineServletStub servlet = new RefineServletStub();
         ImportingManager.initialize(servlet);
         ObjectNode options = ParsingUtilities.evaluateJsonStringToObjectNode(json);
-        ImportingJob job = Mockito.spy(ImportingManager.createJob());
-        Mockito.when(job.getRetrievalRecord()).thenReturn(ParsingUtilities.mapper.createObjectNode());
-        Project project = new Project();
-        ProjectMetadata metadata = new ProjectMetadata();
-        ImportingParserBase parser = new SeparatorBasedImporter();
+        ImportingJob job = ImportingManager.createJob();
         try {
-            parser.parseOneFile(project, metadata, job, "file-source", new StringReader(csv), -1, options,
-                    new ArrayList<>());
-            project.update();
+            new SeparatorBasedImporter().parseOneFile(new Project(), new ProjectMetadata(),
+                    job, "file-source", new StringReader(csv),
+                    -1, options, new ArrayList<>());
         } catch (Throwable t) {
             checker.check(t);
             return;
+        } finally {
+            ImportingManager.disposeJob(job.id);
         }
         throw new AssertionError("Expected exception to be thrown");
     }
